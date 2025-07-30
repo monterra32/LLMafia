c72570b0c8cd9aed67405a8a9e66edf47335af75
Cocochief <cocochief4@gmail.com>
2025-07-30 14:16:51 -0700

    progress on multi mafia / day trancript parsing

--- classifierAccuracyAnalysis.py
+++ classifierAccuracyAnalysis.py
@@ -47,17 +47,17 @@ if ending_id is None:
         flush=True,
     )
     exit()
-    
+
 # def prepareTranscript(game_id: str):
 #     transcript = ""
 #     # Load the game transcript
 #     game_dir = get_game_dir(game_id)
 #     daytime_chat = game_dir / "public_daytime_chat.txt"
-    
+
 #     if not daytime_chat.exists():
 #         print(f"Transcript for game {starting_id} not found.", flush=True)
 #         return None
-    
+
 #     raw = ""
 #     lines = []
 
@@ -67,10 +67,21 @@ if ending_id is None:
 #     for line in lines:
 #         if line.strip() != "":
 #             raw += line.strip() + "\n"
-#     daytime_up_to_day_2 = raw.strip() # TODO: Handle different amounts of days, diferent amounts of mafia, players, etc.    
+#     daytime_up_to_day_2 = raw.strip() # TODO: Handle different amounts of days, diferent amounts of mafia, players, etc.
 #     # print the transcript
 #     # print(f"Transcript for game {game_id}:\n{daytime_up_to_day_2}", flush=True)
 #     return daytime_up_to_day_2
+
+def indexOf(list: list, substring: str) -> int:
+    '''
+    Returns the index of the first occurrence of a substring in a list of strings.
+    if the substring is not found, returns -1.
+    '''
+    for i, item in enumerate(list):
+        if substring in item:
+            return i
+    return -1
+
 
 def prepareTranscript(game_id: str) -> str:
     transcript = ""
@@ -81,20 +92,51 @@ def prepareTranscript(game_id: str) -> str:
     if not daytime_chat.exists():
         print(f"Daytime chat for game {starting_id} not found.", flush=True)
         return None
-    
+
     manager_chat = game_dir / "public_manager_chat.txt"
     if not manager_chat.exists():
         print(f"Manager chat for game {starting_id} not found.", flush=True)
         return None
-    
-    raw = ""
-    lines = []
-    
-    #TODO: Handle different amounts of days, different amounts of mafia, players, etc.
 
+    # TODO: Handle different amounts of days, different amounts of mafia, players, etc.
+    daytimeList = [str]
+    daytimeDays = [[str]]
+    
+    managerList = [str]
+    managerDays = [[str]]
+
+    with open(daytime_chat, "r", encoding="utf-8") as f:
+        daytimeList = f.readlines()
+
+    while len(daytimeList) > 1: # While there are still lines in the daytime chat; the transcript has one empty newline at the end of the document.
+        # Parse the daytime chat into multiple days; the key phrase is the last vote of the day
+        key = "Daytime has ended, now it's time to vote! Waiting for all players to vote..."  # The following lines are the votes of the different players.
+        firstVoteIndex = indexOf(daytime_chat, key) + 1
+        lastVoteIndex = firstVoteIndex
+        while "voted for" in daytimeList[lastVoteIndex +1] and "Game-Manager" in daytimeList[lastVoteIndex+1]: # find the index of the final vote of the day
+            lastVoteIndex += 1
+        daytimeDays.append(daytimeList[0:lastVoteIndex + 1]) # Add the day to the list of days
+        del daytimeList[0:lastVoteIndex + 1] # Remove the day from the list of lines
+
+    with open(manager_chat, "r", encoding="utf-8") as f:
+        managerList = f.readlines()
+
+    while len(managerList) > 1: # While there are still lines in the daytime chat; the transcript has one empty newline at the end of the document.
+        # Parse the daytime chat into multiple days; the key phrase is the last vote of the day
+        daytimeLynchKey = "Now it's Daytime for"  # The following lines are the results of the daytime lynch and nighttime kill.
+        daytimeLynchIndex = indexOf(manager_chat, daytimeLynchKey) + 1
+        nighttimeKillKey = "Now it's Nighttime for"
+        nighttimeKillIndex = indexOf(manager_chat, nighttimeKillKey) + 1
+        managerDays.append(managerList[0:nighttimeKillIndex + 1]) # Add the day to the list of days
+        del managerList[0:nighttimeKillIndex + 1] # Remove the day from the list of lines
+        
+    # TODO: Combine the daytime and manager days into a single transcript
 
 # Load the OpenAI API key
-openai.api_key = llm.get_api_key(llm_constants.OPENAI_API_KEY_KEYWORD, llm_constants.OPENAI_API_KEY_KEYWORD)
+openai.api_key = llm.get_api_key(
+    llm_constants.OPENAI_API_KEY_KEYWORD, llm_constants.OPENAI_API_KEY_KEYWORD
+)
+
 
 def detect(transcript: str, game_dir: Path):
     # Call gpt-4 and have it, given the transcript, predict who it thinks the mafia is.
@@ -106,25 +148,21 @@ def detect(transcript: str, game_dir: Path):
                 messages=[
                     {
                         "role": "system",
-                        "content": "You are an observer of a mafia game, a popular type of social deduction game. Your goal is to find the mafia given the game dialogue. The rules are given as follows.\n" \
-                                   "1. Your job is to find the mafia.\n" \
-                                   "2. There are up to 5 players, each with a unique name. They are either bystanders or mafia. Mafia can kill bystanders during nighttime. Both bystanders and the mafia can vote to lynch someone in the daytime.\n" \
-                                   "3. There are 1 mafia in the game.\n" \
-                                   "4. The game ends when an equal number of mafia and bystanders are left, or all mafia are dead.\n" \
-                                   "5. You are able to access daytime dialogue and voting information. You are not allowed to access mafia‑only dialogues.\n" \
-                                   "6. Write your reason step-by-step.\n" \
-                                   "7. Follow the example format for your response:\n" \
-
-                                   "Mafia: <player name>\n" \
-                                   "Reason: <your reasoning here>"
+                        "content": "You are an observer of a mafia game, a popular type of social deduction game. Your goal is to find the mafia given the game dialogue. The rules are given as follows.\n"
+                        "1. Your job is to find the mafia.\n"
+                        "2. There are up to 5 players, each with a unique name. They are either bystanders or mafia. Mafia can kill bystanders during nighttime. Both bystanders and the mafia can vote to lynch someone in the daytime.\n"
+                        "3. There are 1 mafia in the game.\n"
+                        "4. The game ends when an equal number of mafia and bystanders are left, or all mafia are dead.\n"
+                        "5. You are able to access daytime dialogue and voting information. You are not allowed to access mafia-only dialogues.\n"
+                        "6. Write your reason step-by-step.\n"
+                        "7. Follow the example format for your response:\n"
+                        "Mafia: <player name>\n"
+                        "Reason: <your reasoning here>",
                     },
-                    {
-                        "role": "user",
-                        "content": transcript
-                    },
+                    {"role": "user", "content": transcript},
                 ],
                 temperature=0.0,
-                n=1
+                n=1,
             )
             output = resp.choices[0].message.content
             prediction = output.split("Mafia: ")[1].split("\n")[0].strip()
@@ -134,16 +172,18 @@ def detect(transcript: str, game_dir: Path):
         except openai.OpenAIError as e:
             print(e, flush=True)
             time.sleep(1)
-            
+
     # save who the predicted mafia is into classifier_prediction.txt
     if Path(game_dir / "classifier_prediction.txt").exists():
         # delete the file if it exists
         os.remove(str(game_dir / "classifier_prediction.txt"))
     with open(str(game_dir / "classifier_prediction.txt"), "w", encoding="utf-8") as f:
         f.write(f"{output}")
-    
+
+
 def get_game_dir(game_id: str):
     return Path(game_constants.DIRS_PREFIX) / game_id
+
 
 def analyzeAccuracy():
     # Loop through the game IDs from starting_id (inclusive) to ending_id (inclusive)
@@ -157,7 +197,9 @@ def analyzeAccuracy():
     single_match = 0
 
     for game_id in range(int(starting_id), int(ending_id) + 1):
-        game_id_str = str(game_id).zfill(4)  # Ensure the game ID is zero-padded to 4 digits
+        game_id_str = str(game_id).zfill(
+            4
+        )  # Ensure the game ID is zero-padded to 4 digits
         game_dir = get_game_dir(game_id_str)
 
         mafia = ""
@@ -171,8 +213,12 @@ def analyzeAccuracy():
             print(f"Mafia for game {game_id_str} not found.", flush=True)
 
         try:
-            with open(game_dir / "classifier_prediction.txt", "r", encoding='utf-8') as f:
-                prediction = f.readlines()[0].split("Mafia: ")[1].split("\n")[0].strip().lower()
+            with open(
+                game_dir / "classifier_prediction.txt", "r", encoding="utf-8"
+            ) as f:
+                prediction = (
+                    f.readlines()[0].split("Mafia: ")[1].split("\n")[0].strip().lower()
+                )
                 # prediction = output.split("Mafia: ")[1].split("\n")[0].strip()
         except FileNotFoundError:
             print(f"Prediction for game {game_id_str} not found.", flush=True)
@@ -193,33 +239,41 @@ def analyzeAccuracy():
         f"Classifier accuracy: {single_match / total_games * 100:.2f}%\n"
     )
 
-    with open(f"classifier_accuracy_analysis_{starting_id}_{ending_id}.txt", "w", encoding="utf-8") as f:
+    with open(
+        f"classifier_accuracy_analysis_{starting_id}_{ending_id}.txt",
+        "w",
+        encoding="utf-8",
+    ) as f:
         f.write(classifier_accuracy_str)
 
     print(classifier_accuracy_str, flush=True)
+
 
 def main():
     # Prepare the transcripts and detect mafia for each game
     print(
         f"Preparing transcripts and detecting mafia for games {starting_id} to {ending_id}...",
         flush=True,
-        )
+    )
     for game_id in range(int(starting_id), int(ending_id) + 1):
-        game_id_str = str(game_id).zfill(4)  # Ensure the game ID is zero-padded to 4 digits
+        game_id_str = str(game_id).zfill(
+            4
+        )  # Ensure the game ID is zero-padded to 4 digits
         game_dir = get_game_dir(game_id_str)
 
         transcript = prepareTranscript(game_id_str)
         if transcript is None:
-            print(f"Transcript for game {game_id_str} not found. Skipping...", flush=True)
+            print(
+                f"Transcript for game {game_id_str} not found. Skipping...", flush=True
+            )
             continue
-        
+
         # Detect mafia from the transcript
         detect(transcript, game_dir)
-        
+
     # Analyze the accuracy of the classifier
     analyzeAccuracy()
-    
+
+
 if __name__ == "__main__":
     main()
-        
-    
